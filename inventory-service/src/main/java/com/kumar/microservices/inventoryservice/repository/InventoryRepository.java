9836fc14aa34eb2e31caa7ba809993d6a8651a5a
Dk2698 <dileepkumar1823@gmail.com>
2024-04-18 00:39:10 +0530

    setup inventory-service

---
+++ inventory-service/src/main/java/com/kumar/microservices/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.kumar.microservices.inventoryservice.repository;
+
+
+import com.kumar.microservices.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, int quantity);
+}
---
+++ inventory-service/src/main/java/com/kumar/microservices/inventoryservice/service/InventoryService.java
@@ -0,0 +1,18 @@
+package com.kumar.microservices.inventoryservice.service;
+
+import com.kumar.microservices.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode, Integer quantity) {
+        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+    }
+}
